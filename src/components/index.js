4a9584f505a3f67253882696715bab631982b73f
Sandhya288 <91210210+Sandhya288@users.noreply.github.com>
2023-11-22 21:51:02 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Dashboard from './dashboard';
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
